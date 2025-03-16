6509fe94226405f389d201111c7747958e4f8006
Peter Chege <peterkagure@gmail.com>
2025-03-16 11:13:19 +0300

    Optimize modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,40 +5,11 @@ import {SharedModule} from "../shared/shared/shared.module";
 import {HttpExceptionFilter} from "../shared/filter/http.filter";
 import {APP_FILTER} from "@nestjs/core";
 import {FaceModule} from "../features/face/face.module";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {ConfigService} from "@nestjs/config";
-import {CloudinaryModule} from "nestjs-cloudinary";
 
 @Module({
     imports: [
         SharedModule,
         FaceModule,
-        TypeOrmModule.forRootAsync({
-            imports: [SharedModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => {
-                const dbUrl = configService.get<string>('DATABASE_URL')
-                return {
-                    type: 'mysql',
-                    url: dbUrl,
-                    synchronize: true,
-                    entities: ['dist/src/shared/entity/*.entity.{js,ts}'],
-                    logging: ["error"],
-                }
-            },
-        }),
-        CloudinaryModule.forRootAsync({
-            imports: [SharedModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => {
-                return {
-                    isGlobal: true,
-                    cloud_name: configService.get('CLOUD_NAME'),
-                    api_key: configService.get('CLOUDINARY_API_KEY'),
-                    api_secret: configService.get('CLOUDINARY_API_SECRET'),
-                }
-            }
-        }),
     ],
     controllers: [AppController],
     providers: [
--- src/shared/shared/shared.module.ts
+++ src/shared/shared/shared.module.ts
@@ -9,6 +9,7 @@ import {AwsSdkModule} from "aws-sdk-v3-nest";
 import {RekognitionClient} from "@aws-sdk/client-rekognition";
 import {CloudinaryRepositoryService} from "../cloudinary/cloudinary-repository.service";
 import {FaceRepositoryService} from "../repository/face-repository.service";
+import {CloudinaryModule} from "nestjs-cloudinary";
 
 
 @Module({
@@ -23,8 +24,32 @@ import {FaceRepositoryService} from "../repository/face-repository.service";
         HttpModule.register({
             maxRedirects: 2,
         }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                const dbUrl = configService.get<string>('DATABASE_URL')
+                const shouldSynchronize = configService.get("NODE_ENV") !== "production"
+                return {
+                    type: 'mysql',
+                    url: dbUrl,
+                    synchronize: shouldSynchronize,
+                    entities: ['dist/src/shared/entity/*.entity.{js,ts}'],
+                    logging: ["error"],
+                }
+            },
+        }),
+        CloudinaryModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    isGlobal: true,
+                    cloud_name: configService.get('CLOUD_NAME'),
+                    api_key: configService.get('CLOUDINARY_API_KEY'),
+                    api_secret: configService.get('CLOUDINARY_API_SECRET'),
+                }
+            }
+        }),
         AwsSdkModule.registerAsync({
-            imports: [ConfigModule],
             inject: [ConfigService],
             clientType: RekognitionClient,
             useFactory: (configService: ConfigService) => {
